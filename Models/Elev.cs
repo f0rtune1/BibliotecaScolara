521e1dcc69a2149b0c666485700ff5ceb58c2c2c
Fortune <papraileanuadrian@gmail.com>
2026-04-26 14:24:28 +0300

    Add Elev model

---
+++ Models/Elev.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace BibliotecaScolara.Models
+{
+    public class Elev
+    {
+        public int IDElev { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Clasa { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public DateTime DataInscrierii { get; set; }
+        public string Status { get; set; }
+        public DateTime DataAdaugarii { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nume} {Prenume} - Clasa {Clasa}";
+        }
+    }
+}
